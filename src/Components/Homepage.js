f1f6fc1e310494b6779aea17c5a8189e24df3615
AntoniaDobrescu <antonia.dobrescu91@gmail.com>
2019-11-22 12:20:46 +0200

    erased the alive label

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -64,7 +64,7 @@ class Homepage extends React.Component {
                 firstName: this.state.firstName,
                 lastName: this.state.lastName,
                 age: this.state.age,
-                alive: this.state.alive,
+                //    alive: this.state.alive,
                 gender: this.state.gender,
                 lookingFor: this.state.lookingFor,
                 homeworld: this.state.homeworld,
@@ -78,68 +78,69 @@ class Homepage extends React.Component {
         )
     }
 
-sendData = () => {
-    this.props.saveJson(this.state.json);
-}
+    sendData = () => {
+        this.props.saveJson(this.state.json);
+    }
 
     render() {
         console.log('this.state: ', this.state)
         return (
-            <Card className='cardContainer'>
-                <Card.Body>
-                    <Form>
-                        <Form.Group controlId="formGroupFirstName" >
-                            <Form.Label><h4>First Name</h4></Form.Label>
-                            <Form.Control type="text" size='lg' name="firstName" onChange={this.handleChange} />
-                        </Form.Group>
+            <div class="soldier">
+                <Card className='cardContainer'>
+                    <Card.Body>
+                        <Form>
+                            <Form.Group controlId="formGroupFirstName" >
+                                <Form.Label><h4>First Name</h4></Form.Label>
+                                <Form.Control type="text" size='lg' name="firstName" onChange={this.handleChange} />
+                            </Form.Group>
 
-                        <Form.Group controlId="formGroupLastName">
-                            <Form.Label><h4>Last Name</h4></Form.Label>
-                            <Form.Control type="text" size='lg' name='lastName' onChange={this.handleChange} />
-                        </Form.Group>
+                            <Form.Group controlId="formGroupLastName">
+                                <Form.Label><h4>Last Name</h4></Form.Label>
+                                <Form.Control type="text" size='lg' name='lastName' onChange={this.handleChange} />
+                            </Form.Group>
 
-                        <Form.Group controlId="formGroupPassword">
-                            <Form.Label><h4>Password</h4></Form.Label>
-                            <Form.Control type="password" size='lg' />
-                        </Form.Group>
+                            <Form.Group controlId="formGroupPassword">
+                                <Form.Label><h4>Password</h4></Form.Label>
+                                <Form.Control type="password" size='lg' />
+                            </Form.Group>
 
-                        <Form.Group controlId="formGroupConfirmPassword">
-                            <Form.Label><h4>Confirm Password</h4></Form.Label>
-                            <Form.Control type="password" size='lg' />
-                        </Form.Group>
+                            <Form.Group controlId="formGroupConfirmPassword">
+                                <Form.Label><h4>Confirm Password</h4></Form.Label>
+                                <Form.Control type="password" size='lg' />
+                            </Form.Group>
 
-                        <Form.Group controlId="formGroupAge">
-                            <Form.Label><h4>Age</h4></Form.Label>
-                            <Form.Control type="number" size='lg' name='age' onChange={this.handleChange} />
-                        </Form.Group>
+                            <Form.Group controlId="formGroupAge">
+                                <Form.Label><h4>Age</h4></Form.Label>
+                                <Form.Control type="number" size='lg' name='age' onChange={this.handleChange} />
+                            </Form.Group>
 
-                        <div className='sectionChoose'>
+                            {/* <div className='sectionChoose'>
                             <h4>Alive</h4>
                             <div>
                                 <input type="radio" className='radio' name='alive' value='yes' onChange={this.handleChange} />  <label htmlFor='yesLabel' className='sectionChooseText'>Yes</label>
                                 <input type="radio" className='radio' name='alive' value='no' onChange={this.handleChange} />  <label htmlFor='noLabel' className='sectionChooseText'>No</label>
                             </div>
-                        </div>
+                        </div> */}
 
-                        <div className='sectionChoose'>
-                            <h4>Gender</h4>
-                            <div>
-                                <input id="maleLabel" type="radio" name="gender" value="male" className='radio' onChange={this.handleChange} />  <label htmlFor="maleLabel" className='sectionChooseText'>Male</label>
-                                <input id="femaleLabel" type="radio" name="gender" value="female" className='radio' onChange={this.handleChange} />  <label htmlFor="femaleLabel" className='sectionChooseText'>Female</label>
+                            <div className='sectionChoose'>
+                                <h4>Gender</h4>
+                                <div>
+                                    <input id="maleLabel" type="radio" name="gender" value="male" className='radio' onChange={this.handleChange} />  <label htmlFor="maleLabel" className='sectionChooseText'>Male</label>
+                                    <input id="femaleLabel" type="radio" name="gender" value="female" className='radio' onChange={this.handleChange} />  <label htmlFor="femaleLabel" className='sectionChooseText'>Female</label>
+                                </div>
+
                             </div>
 
-                        </div>
-
-                        <div className='sectionChoose'>
-                            <h4>Looking For</h4>
-                            <div>
-                                <input type="radio" name="lookingFor" value="male" className='radio' onChange={this.handleChange} />  <label htmlFor="maleLabel" className='sectionChooseText'>Male</label>
-                                <input type="radio" name="lookingFor" value="female" className='radio' onChange={this.handleChange} />  <label htmlFor="femaleLabel" className='sectionChooseText'>Female</label>
-                                <input type="radio" name="lookingFor" value="both" className='radio' onChange={this.handleChange} />  <label htmlFor="bothLabel" className='sectionChooseText'>Both</label>
+                            <div className='sectionChoose'>
+                                <h4>Looking For</h4>
+                                <div>
+                                    <input type="radio" name="lookingFor" value="male" className='radio' onChange={this.handleChange} />  <label htmlFor="maleLabel" className='sectionChooseText'>Male</label>
+                                    <input type="radio" name="lookingFor" value="female" className='radio' onChange={this.handleChange} />  <label htmlFor="femaleLabel" className='sectionChooseText'>Female</label>
+                                    <input type="radio" name="lookingFor" value="both" className='radio' onChange={this.handleChange} />  <label htmlFor="bothLabel" className='sectionChooseText'>Both</label>
+                                </div>
                             </div>
-                        </div>
 
-                        {/* <Form.Group controlId="formGroupHomeworld">
+                            {/* <Form.Group controlId="formGroupHomeworld">
                             <Form.Label><h4>Homeworld</h4></Form.Label>
                             <Form.Control as="select" size='lg'>
                                 <option>Choose...</option>
@@ -147,35 +148,39 @@ sendData = () => {
                             </Form.Control>
                         </Form.Group> */}
 
-                        <Form.Group controlId="formGroupAge">
-                            <Form.Label><h4>Homeworld</h4></Form.Label>
-                            <Form.Control type="text" size='lg' name='homeworld' onChange={this.handleChange} />
-                        </Form.Group>
+                            <Form.Group controlId="formGroupAge">
+                                <Form.Label><h4>Homeworld</h4></Form.Label>
+                                <Form.Control type="text" size='lg' name='homeworld' onChange={this.handleChange} />
+                            </Form.Group>
 
-                        <div className='sectionChoose'>
-                            <h4>Jedi</h4>
-                            <div>
-                                <input type="radio" name="jedi" value="yes" className='radio' onChange={this.handleChange} />  <label htmlFor='yesLabel' className='sectionChooseText'>Yes</label>
-                                <input type="radio" name="jedi" value="no" className='radio' onChange={this.handleChange} />  <label htmlFor='noLabel' className='sectionChooseText'>No</label>
+                            <div className='sectionChoose'>
+                                <h4>Jedi</h4>
+                                <div>
+                                    <input type="radio" name="jedi" value="yes" className='radio' onChange={this.handleChange} />  <label htmlFor='yesLabel' className='sectionChooseText'>Yes</label>
+                                    <input type="radio" name="jedi" value="no" className='radio' onChange={this.handleChange} />  <label htmlFor='noLabel' className='sectionChooseText'>No</label>
+                                </div>
                             </div>
-                        </div>
 
-                        <div className='sectionChoose'>
-                            <h4>Affiliation</h4>
-                            <div>
-                                <input type="radio" name="affiliation" value="resistance" className='radio' onChange={this.handleChange} />  <label htmlFor='' className='sectionChooseText'>Resistance</label>
-                                <input type="radio" name="affiliation" value="empire" className='radio' onChange={this.handleChange} />  <label htmlFor='' className='sectionChooseText'>Empire</label>
+                            <div className='sectionChoose'>
+                                <h4>Affiliation</h4>
+                                <div>
+                                    <input type="radio" name="affiliation" value="resistance" className='radio' onChange={this.handleChange} />  <label htmlFor='' className='sectionChooseText'>Resistance</label>
+                                    <input type="radio" name="affiliation" value="empire" className='radio' onChange={this.handleChange} />  <label htmlFor='' className='sectionChooseText'>Empire</label>
+                                </div>
                             </div>
-                        </div>
 
-                        <div className='containerButtons'>
-                            <Button variant="secondary" className='button'>Upload Image</Button>
-                            <Button variant="secondary" className='button'>Link Facebook</Button>
-                            <Button variant="primary" className='button' onClick={this.handleSubmit}>Sign Up</Button>
-                        </div>
-                    </Form>
-                </Card.Body>
-            </Card>
+                            <div className='containerButtons'>
+                                <Button variant="secondary" className='button'>Upload Image</Button>
+                                <Button variant="secondary" className='button'>Link Facebook</Button>
+                                <Button variant="primary" className='button' onClick={this.handleSubmit}>Sign Up</Button>
+                            </div>
+                        </Form>
+                    </Card.Body>
+                </Card>
+            </div>
+
+
+
         );
     }
 }
