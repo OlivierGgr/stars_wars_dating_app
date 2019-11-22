701b6ceab7e4b38bc0c2c0343e1110da723b8d1a
Cosmin Andrei <cosminandrei.cnt@gmail.com>
2019-11-22 16:34:43 +0200

    Updates

---
+++ precache-manifest.f87363e485b88bca92173b87dc47066e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc8b71499e325bbdd6294a890eb76f4",
+    "url": "/bucharest-hackathon1-19nov-gr1/index.html"
+  },
+  {
+    "revision": "f1dc9b136f6291778335",
+    "url": "/bucharest-hackathon1-19nov-gr1/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "462d12ea38a7b4a0c0c2",
+    "url": "/bucharest-hackathon1-19nov-gr1/static/css/main.cb6a0487.chunk.css"
+  },
+  {
+    "revision": "f1dc9b136f6291778335",
+    "url": "/bucharest-hackathon1-19nov-gr1/static/js/2.762e830a.chunk.js"
+  },
+  {
+    "revision": "462d12ea38a7b4a0c0c2",
+    "url": "/bucharest-hackathon1-19nov-gr1/static/js/main.b4b51d39.chunk.js"
+  },
+  {
+    "revision": "0c194594f321e796ec19",
+    "url": "/bucharest-hackathon1-19nov-gr1/static/js/runtime-main.ab92f2f1.js"
+  },
+  {
+    "revision": "6754cd847958a032adafbd093f1af58c",
+    "url": "/bucharest-hackathon1-19nov-gr1/static/media/TFA2.6754cd84.jpg"
+  },
+  {
+    "revision": "766ca1eff3000e6450a491bd5838ffba",
+    "url": "/bucharest-hackathon1-19nov-gr1/static/media/noMatches.766ca1ef.png"
+  }
+]);
